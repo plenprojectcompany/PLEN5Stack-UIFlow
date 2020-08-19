539612f2ccce495f2063c0de7df1e2c94cfd565a
IsakiMatsubara <isakimatsubara@gmail.com>
2020-08-19 14:37:16 +0900

    ライブラリーの更新

--- PythonCode/SetupBlock.py
+++ PythonCode/SetupBlock.py
@@ -6,7 +6,9 @@ import machine, neopixel
 
 #変数の定義
 ServoDefaultValue = [1000, 630, 300, 600, 240, 600, 1000, 720]
-MotionCache = []
+MotionNumberCache = []
+TransitionTimeArrayCache = []
+SearvoArrayCache = []
 CurrentLEDValue = [(0,0,0),(0,0,0)]
 ServoCurrentValue = [0] * 8
 ServoBeforeValue = [0] * 8
@@ -49,7 +51,7 @@ def setAngle(angle,time):
   global MotionCount
   MotionCount+=1
 
-
+#初期化
 write8(0xFE, 0x85)
 write8(0xFA, 0x00)
 write8(0xFB, 0x00)
@@ -57,47 +59,49 @@ write8(0xFC, 0x66)
 write8(0xFD, 0x00)
 write8(0x00, 0x01)
 setAngle([0, 0, 0, 0, 0, 0, 0, 0], 10)
+CurrentLEDValue[0] = [50, 0, 0]
+CurrentLEDValue[1] = [50, 0, 0]
+np[0] = CurrentLEDValue[0]
+np[1] = CurrentLEDValue[1]
+np.write()
 
 def MotionStart(MotionNumber):
     global MotionCount
 
-    #キャッシュされているか確認
-    Cache = ""
-    for i in range(len(MotionCache) / 2):
-      if(int(MotionCache[i * 2]) == MotionNumber):
-        Cache = MotionCache[i * 2 + 1]
-        break
-
-    if(Cache == ""):
-      ReadByteFrom = 50 + 860 * MotionNumber
+    if(MotionNumber in MotionNumberCache): #キャッシュされているか確認
+      CacheNumber = MotionNumberCache.index(MotionNumber)
+      #キャッシュから取得する
+      TransitionTimeArray = TransitionTimeArrayCache[CacheNumber]
+      SearvoArray = SearvoArrayCache[CacheNumber]
+    else:
       #モーションデータの読み取り
+      ReadByteFrom = 50 + 860 * MotionNumber
       _data = bytearray(2)
       _data[0] = ReadByteFrom >> 8
       _data[1] = ReadByteFrom & 0xFF
       i2c.writeto(0x56, _data)
       ReadData = i2c.readfrom(0x56, 860)
-      MotionCache.append(str(MotionNumber))
-      MotionCache.append(str(ReadData))
-    else:
-      ReadData = Cache
 
-    #モーションデータの切り出し
-    MotionDataArray = str(ReadData).split('>')
-    TransitionTimeArray = []
-    SearvoArray = []
+      #モーションデータの切り出し
+      MotionDataArray = str(ReadData).split('>')
+      TransitionTimeArray = []
+      SearvoArray = []
 
-    for i in MotionDataArray:
-        if(re.match('^MF' + '{:02x}'.format(MotionNumber),i)):
-            check1=re.match('(MF....)(....)',i)
-            TransitionTimeArray.append(int(check1.group(2),16))
-            for n in range(8):
-                check2 = int(i[10+4*n:10+4*n+4],16)
-                if check2 >= 0x7fff:
-                  check2 = ~(~check2 & 0xffff)
-                else:
-                  check2 = check2 & 0xffff
-                SearvoArray.append(check2)
-
+      for i in MotionDataArray:
+          if(re.match('^MF' + '{:02x}'.format(MotionNumber),i)):
+              check1=re.match('(MF....)(....)',i)
+              TransitionTimeArray.append(int(check1.group(2),16))
+              for n in range(8):
+                  check2 = int(i[10+4*n:10+4*n+4],16)
+                  if check2 >= 0x7fff:
+                    check2 = ~(~check2 & 0xffff)
+                  else:
+                    check2 = check2 & 0xffff
+                  SearvoArray.append(check2)
+      #読み込んだデータはキャッシュする
+      MotionNumberCache.append(MotionNumber)
+      TransitionTimeArrayCache.append(TransitionTimeArray)
+      SearvoArrayCache.append(SearvoArray)
 
     #サーボモーターを動かす
     ErrorFlag = False
@@ -106,14 +110,11 @@ def MotionStart(MotionNumber):
       for i in range(8):
         count1 = 8 * MotionCount + i
         SearvoArrayCheck.append(SearvoArray[count1])
-      if(SearvoArrayCheck == ServoBeforeValue): #同じサーボ角を繰り返す場合、動作スキップする
+      if(ServoBeforeValue == SearvoArrayCheck): #同じサーボ角を繰り返す場合、動作スキップする
         MotionCount+=1
       else:
         MotionCountBefore = MotionCount
-        if(SearvoArrayCheck == [0,0,0,0,0,0,0,0]): #サーボを初期値に戻す場合、動作を高速化する
-          setAngle(SearvoArrayCheck,10)
-        else:
-          setAngle(SearvoArrayCheck,TransitionTimeArray[MotionCount])
+        setAngle(SearvoArrayCheck,TransitionTimeArray[MotionCount])
         while(MotionCount == MotionCountBefore):
           wait_ms(1)
       for i in range(8):
